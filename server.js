fafc10a1d53b4b20a3fad5b34d9fdb0cf0fedfc5
Kevin Reber <kevinreber1@gmail.com>
2024-08-20 21:09:43 -0700

    initialize prisma when app starts

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import { createRequestHandler } from "@remix-run/express";
 import express from "express";
+import { prisma } from "./services/prisma.server";
+// import * as build from "./build/server/index.js";
 
 // notice that the result of `remix vite:build` is "just a module"
-import * as build from "./build/server/index.js";
 
 const viteDevServer =
   process.env.NODE_ENV === "production"
@@ -16,20 +17,32 @@ const viteDevServer =
 const app = express();
 
 app.use(
-  viteDevServer
-    ? viteDevServer.middlewares
-    : express.static("build/client")
+  viteDevServer ? viteDevServer.middlewares : express.static("build/client")
 );
 
-const build = viteDevServer
-  ? () =>
-      viteDevServer.ssrLoadModule(
-        "virtual:remix/server-build"
-      )
-  : await import("./build/server/index.js");
+// const build = viteDevServer
+//   ? () =>
+//       viteDevServer.ssrLoadModule(
+//         "virtual:remix/server-build"
+//       )
+//   : await import("./build/server/index.js");
 
 // and your app is "just a request handler"
-app.all("*", createRequestHandler({ build }));
+// app.all("*", createRequestHandler({ build }));
+
+const getBuild = viteDevServer
+  ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
+  : await import("./build/server/index.js");
+
+app.all(
+  "*",
+  createRequestHandler({
+    build: await getBuild(),
+    getLoadContext() {
+      return { prisma }; // Pass Prisma client into Remix context
+    },
+  })
+);
 
 app.listen(3000, () => {
   console.log("App listening on http://localhost:3000");
