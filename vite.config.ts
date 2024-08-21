974b612b3c8ab703b35e9400f11fe03e6fee65c0
Kevin Reber <kevinreber1@gmail.com>
2024-08-20 23:15:05 -0700

    setup sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vercelPreset } from "@vercel/remix/vite";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      presets: [vercelPreset()],
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [remix({
+    presets: [vercelPreset()],
+    future: {
+      v3_fetcherPersist: true,
+      v3_relativeSplatPath: true,
+      v3_throwAbortReason: true,
+    },
+  }), tsconfigPaths(), sentryVitePlugin({
+    org: "kevin-reber",
+    project: "pixel-studio"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
