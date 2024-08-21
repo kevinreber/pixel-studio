bea1b601b327b26bf4bdf671b6c706b9acd1c18f
Kevin Reber <kevinreber1@gmail.com>
2024-08-20 20:21:46 -0700

    added data fetcher

--- app/root.tsx
+++ app/root.tsx
@@ -4,10 +4,21 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { prisma } from "./services/prisma.server";
+// import { LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async () => {
+  const users = await prisma.user.findMany();
+  return users;
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const data = useLoaderData();
+  console.log(data);
+
   return (
     <html lang="en">
       <head>
