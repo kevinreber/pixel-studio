39c91873ed95730379301effbda8b3f4ff47d3ef
Kevin Reber <kevinreber1@gmail.com>
2024-09-18 21:46:53 -0400

    updated auth for root and login

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,6 @@ import {
 } from "@remix-run/react";
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { Toaster, toast as showToast } from "sonner";
-import { prisma } from "services/prisma.server";
 import NavigationSidebar from "components/NavigationSidebar";
 import { csrf } from "./utils/csrf.server";
 import { getEnv } from "./utils/env.server";
@@ -22,44 +21,45 @@ import { AuthenticityTokenProvider } from "remix-utils/csrf/react";
 import { HoneypotProvider } from "remix-utils/honeypot/react";
 import { honeypot } from "utils/honeypot.server";
 import { getToast, type Toast } from "utils/toast.server";
-import { getSupabaseEnv,  getSupabaseWithSessionAndHeaders } from "./services/supabase.server";
-import { useSupabaseClient } from "./hooks/useSupabaseClient";
+import {
+  sessionStorage,
+  getSessionCookie,
+  authenticator,
+  getGoogleSessionAuth,
+  USER_ID_KEY,
+} from "./services";
 
 import "./tailwind.css";
 import "./globals.css";
 
 export async function loader({ request }: LoaderFunctionArgs) {
+  const user = await authenticator.isAuthenticated(request);
+
+  const sessionAuth = await getGoogleSessionAuth(request);
+  if (sessionAuth) {
+    const cookieSession = await getSessionCookie(request);
+    cookieSession.set(USER_ID_KEY, sessionAuth.id);
+    await sessionStorage.commitSession(cookieSession);
+  }
+
   const [csrfToken, csrfCookieHeader] = await csrf.commitToken(request);
   const honeyProps = honeypot.getInputProps();
-  const users = (await prisma.user.findMany()) || [];
 
   const { toast, headers: toastHeaders } = await getToast(request);
 
-
-  const { serverSession, headers: supabaseHeaders } = await getSupabaseWithSessionAndHeaders({
-    request,
-  });
-
-  const { DATABASE_URL, DATABASE_ANON_KEY } = getSupabaseEnv()
-
   return json(
     {
-      // username: os.userInfo().username,
-      // theme: getTheme(request),
-      users: users.length,
+      user,
       toast,
       ENV: getEnv(),
       csrfToken,
       honeyProps,
-      DATABASE_URL,
-      DATABASE_ANON_KEY,
-      serverSession,
-      domainUrl: process.env.ORIGIN || '',
+      domainUrl: process.env.ORIGIN || "",
     },
     {
       headers: combineHeaders(
         csrfCookieHeader ? { "set-cookie": csrfCookieHeader } : null,
-        toastHeaders, supabaseHeaders
+        toastHeaders
       ),
     }
   );
@@ -67,10 +67,10 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 function Document({
   children,
-  env,
-}: {
+}: // env,
+{
   children: React.ReactNode;
-  env?: Record<string, string>;
+  // env?: Record<string, string>;
 }) {
   return (
     <html lang="en" className="dark h-full overflow-x-hidden">
@@ -80,14 +80,13 @@ function Document({
         <Meta />
         <Links />
       </head>
-      {/* <body className="dark"> */}
       <body>
         {children}
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `window.ENV = ${JSON.stringify(env)}`,
           }}
-        />
+        /> */}
         <Toaster closeButton position="top-center" />
         <ScrollRestoration />
         <Scripts />
@@ -97,40 +96,31 @@ function Document({
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const data = useLoaderData<typeof loader>();
-  
+  const loaderData = useLoaderData<typeof loader>();
+  console.log(loaderData);
+  const location = useLocation();
+  const isHome = location.pathname === "/";
 
-console.log(data);
-const location = useLocation();
-const isHome = location.pathname === "/";
-
-return (
-  <>
+  return (
+    <>
+      {/* <Document env={loaderData.ENV}> */}
       <Document>
-      {/* <Document env={data.ENV}> */}
         {!isHome && <NavigationSidebar />} {children}
-        {data && data.toast ? <ShowToast toast={data.toast} /> : null}
+        {loaderData && loaderData.toast ? (
+          <ShowToast toast={loaderData.toast} />
+        ) : null}
       </Document>
     </>
   );
 }
 
 export default function App() {
-  const data = useLoaderData<typeof loader>();
-  const {supabase} = useSupabaseClient(
-  {
-  env: {
-    SUPABASE_URL: data.DATABASE_URL,
-    SUPABASE_ANON_KEY: data.DATABASE_ANON_KEY,
-  },
-  serverSession: data.serverSession,
-  })
+  const loaderData = useLoaderData<typeof loader>();
 
   return (
-    <HoneypotProvider {...data.honeyProps}>
-      <AuthenticityTokenProvider token={data.csrfToken}>
-        {/* <Outlet />; */}
-        <Outlet context={{ supabase, domainUrl: data.domainUrl }} />
+    <HoneypotProvider {...loaderData.honeyProps}>
+      <AuthenticityTokenProvider token={loaderData.csrfToken}>
+        <Outlet context={{ user: loaderData.user }} />;
       </AuthenticityTokenProvider>
     </HoneypotProvider>
   );
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,106 +1,41 @@
 import PageContainer from "components/PageContainer";
 import PixelStudioIcon from "components/PixelStudioIcon";
 import type { LoaderFunctionArgs } from "@remix-run/node";
-// import GoogleLoginButton from "../components/GoogleLoginButton";
+import GoogleLoginButton from "../components/GoogleLoginButton";
 import { Card } from "@/components/ui/card";
-import { json, redirect, useOutletContext } from "@remix-run/react";
-import { getSupabaseWithSessionAndHeaders } from "~/services/supabase.server";
-import { Button } from "@/components/ui/button";
+import { json } from "@remix-run/react";
+// import { getSupabaseWithSessionAndHeaders } from "~/services/supabase.server";
+import { requireAnonymous } from "~/services";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const { headers, serverSession } = await getSupabaseWithSessionAndHeaders({
-    request,
-  });
+  await requireAnonymous(request);
 
-  if (serverSession) {
-    return redirect('/explore', { headers });
-  }
-
-  return json({ success: true });
-  // return json({ success: true }, { headers });
-  // If 'request' is not used, we can remove it from the destructuring
-  return {};
+  return json({});
 }
 
 export default function Index() {
-  // const { supabase, domainUrl } = useOutletContext<SupabaseOutletContext>();
-  const { supabase, domainUrl } = useOutletContext<{ supabase: Record<string, any>; domainUrl: string }>();
-
-
-  const handleSignIn = async () => {
-    console.log('clicked login......');
-    try {
-      await supabase.auth.signInWithOAuth({
-        provider: 'google',
-      options: {
-        redirectTo: `${domainUrl}/auth/callback`,
-      },
-    });
-  } catch (error) {
-    console.error('Error signing in with Google:', error);
-  }
-  };
-
-    return (
-      <PageContainer>
-        <div className="flex min-h-full flex-1 flex-col justify-center pb-8 sm:px-6 lg:px-8 pt-0">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md flex flex-col items-center">
-            <div className="w-16 mb-2">
-              <PixelStudioIcon />
-            </div>
-            {/* <h2 className="text-center text-2xl font-bold leading-9 tracking-tight">
-              Pixel Studio AI{" "}
-            </h2> */}
-            <h2 className="text-2xl m-0">Pixel Studio AI</h2>
-  
-            <h1 className="mt-6 text-center text-1xl">Sign in to your account</h1>
+  return (
+    <PageContainer>
+      <div className="flex min-h-full flex-1 flex-col justify-center pb-8 sm:px-6 lg:px-8 pt-0">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md flex flex-col items-center">
+          <div className="w-16 mb-2">
+            <PixelStudioIcon />
           </div>
-  
-          <Card className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-            <div className="bg-[#24292F] px-6 py-12 shadow sm:rounded-lg sm:px-12">
-          
-  
+          <h2 className="text-2xl m-0">Pixel Studio AI</h2>
+          <h1 className="mt-6 text-center text-1xl">Sign in to your account</h1>
+        </div>
 
-  
-              <div>
- 
-  
-                <p className="text-center text-sm text-gray-200">Sign in with</p>
-                <div className="mt-6 w-full">
-                  {/* <GoogleLoginButton /> */}
-                  <Button
-            onClick={handleSignIn}
-                    // className="border-solid border-gray-600 flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
-                    variant="outline"
-                    className="w-full"
-                  >
-                    <svg
-                      className="mr-2 -ml-1 w-4 h-4"
-                      aria-hidden="true"
-                      focusable="false"
-                      data-prefix="fab"
-                      data-icon="google"
-                      role="img"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 488 512"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                      ></path>
-                    </svg>
-                    Google
-                  </Button>
-
-                </div>
+        <Card className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <div className="bg-[#24292F] px-6 py-12 shadow sm:rounded-lg sm:px-12">
+            <div>
+              <p className="text-center text-sm text-gray-200">Sign in with</p>
+              <div className="mt-6 w-full">
+                <GoogleLoginButton />
               </div>
             </div>
-  
-          </Card>
-        </div>
-  
-
-      </PageContainer>
-    );
-  }
-  
+          </div>
+        </Card>
+      </div>
+    </PageContainer>
+  );
+}
