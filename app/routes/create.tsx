8773de4afade0a2fd78d90a8caa22229b3230b90
Kevin Reber <kevinreber1@gmail.com>
2024-11-02 23:44:30 -0700

    updated create image options

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -10,52 +10,112 @@ export const meta: MetaFunction = () => {
 
 const MODEL_OPTIONS = [
   {
-    name: "Dreamshaper XL Lightning",
-    image: "/assets/model-thumbs/ds-xl-lightning.jpg",
-    description: "Dreamshaper XL, accelerated. High quality, fast and cheap.",
+    name: "Stable Diffusion 1.5",
+    value: "stable-diffusion-1-5",
+    image: "/assets/model-thumbs/sd-1-5.jpg",
+    description: "The most popular first-generation stable diffusion model.",
   },
   {
-    name: "Flux",
-    image: "/assets/model-thumbs/flux-dev-thumb-2.jpg",
-    description:
-      "The largest open-source text-to-image model to date, by Black Forest Labs.",
+    name: "Stable Diffusion XL",
+    value: "stable-diffusion-xl",
+    image: "/assets/model-thumbs/sdxlv1.jpg",
+    description: "The state-of-the-art in open-source image generation.",
   },
   {
     name: "DALL-E 2",
+    value: "dall-e",
     image: "/assets/model-thumbs/dalle2.jpg",
     description: "State-of-the-art image generator from OpenAI.",
   },
-  {
-    name: "DALL-E 3",
-    image: "/assets/model-thumbs/dalle3.jpg",
-    description: "State-of-the-art image generator from OpenAI.",
-  },
+  // {
+  //   name: "Dreamshaper XL Lightning",
+  //   image: "/assets/model-thumbs/ds-xl-lightning.jpg",
+  //   description: "Dreamshaper XL, accelerated. High quality, fast and cheap.",
+  // },
+  // {
+  //   name: "Flux",
+  //   image: "/assets/model-thumbs/flux-dev-thumb-2.jpg",
+  //   description:
+  //     "The largest open-source text-to-image model to date, by Black Forest Labs.",
+  // },
+  // {
+  //   name: "DALL-E 3",
+  //   image: "/assets/model-thumbs/dalle3.jpg",
+  //   description: "State-of-the-art image generator from OpenAI.",
+  // },
   // {
   //   name: "Ideogram 2.0",
   //   image: "/assets/model-thumbs/ideogram-v1.jpg",
   //   description: "A model by Ideogram that is amazing at Typography.",
   // },
-  {
-    name: "Google Imagen 3.0",
-    image: "/assets/model-thumbs/imagen-3-0-thumb.jpg",
-    description:
-      "A model by Google DeepMind that is great at typography & prompt adherence.",
-  },
+  // {
+  //   name: "Google Imagen 3.0",
+  //   image: "/assets/model-thumbs/imagen-3-0-thumb.jpg",
+  //   description:
+  //     "A model by Google DeepMind that is great at typography & prompt adherence.",
+  // },
 ];
 
 const STYLE_OPTIONS = [
-  { name: "NightCafe", image: "/assets/preset-text-styles/nightcafe-4.jpg" },
-  { name: "Cinematic", image: "/assets/preset-text-styles/cinematic.jpg" },
-  { name: "Realistic Anime", image: "/assets/preset-text-styles/anime-v2.jpg" },
   {
-    name: "Artistic Portrait",
+    name: "3d Model",
+    value: "3d-model",
+    image: "/assets/preset-text-styles/3d-game-v2.jpg",
+  },
+  {
+    name: "Anime",
+    value: "anime",
+    image: "/assets/preset-text-styles/anime-v2.jpg",
+  },
+  {
+    name: "Cinematic",
+    value: "cinematic",
+    image: "/assets/preset-text-styles/cinematic.jpg",
+  },
+  {
+    name: "Comic Book",
+    value: "comic-book",
+    image: "/assets/preset-text-styles/modern-comic.jpg",
+  },
+  {
+    name: "Digital Art",
+    value: "digital-art",
     image: "/assets/preset-text-styles/artistic-portrait.jpg",
   },
-  { name: "Vibrant", image: "/assets/preset-text-styles/vibrant.jpg" },
   {
-    name: "Epic Origami",
+    name: "Fantasy Art",
+    value: "fantasy-art",
+    image: "/assets/preset-text-styles/fantasy.jpg",
+  },
+  {
+    name: "Neon Punk",
+    value: "neon-punk",
+    image: "/assets/preset-text-styles/cyberpunk.jpg",
+  },
+  {
+    name: "Origami",
+    value: "origami",
     image: "/assets/preset-text-styles/epic-origami.jpg",
   },
+  {
+    name: "Photographic",
+    value: "photographic",
+    image: "/assets/preset-text-styles/photo.jpg",
+  },
+  // { name: "Enhance", image: "/assets/preset-text-styles/.jpg" },
+  // { name: "Isometric", image: "/assets/preset-text-styles/.jpg" },
+  // { name: "Line Art", image: "/assets/preset-text-styles/.jpg" },
+  // { name: "Low Poly", image: "/assets/preset-text-styles/.jpg" },
+  // {
+  //   name: "Modeling Compound",
+  //   image: "/assets/preset-text-styles/.jpg",
+  // },
+  // { name: "Analog Film", image: "/assets/preset-text-styles/jpg" },
+  // { name: "Pixel Art", image: "/assets/preset-text-styles/.jpg" },
+  // {
+  //   name: "Tile Texture",
+  //   image: "/assets/preset-text-styles/.jpg",
+  // },
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
