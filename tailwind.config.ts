ec1d7074f6e192de4738767875afa7d04691140b
Kevin Reber <kevinreber1@gmail.com>
2024-11-11 21:49:51 -0800

    added animation for like button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,16 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        heartBeat: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.2)" },
+          "100%": { transform: "scale(1)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        heartBeat: "heartBeat 0.3s ease-in-out",
       },
     },
   },
