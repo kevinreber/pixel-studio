5d34b5ff1efd0ec8d26d62cc95516a182a4986f1
Kevin Reber <kevinreber1@gmail.com>
2024-11-18 00:00:10 -0800

    disabled playwright tests

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 // const BASE_URL = process.env.ORIGIN;
 
-test.skip("Login page renders", async ({ page }) => {
-  await page.goto("/login");
-  await expect(page).toHaveTitle(/login/i);
-});
+// test("Login page renders", async ({ page }) => {
+// await page.goto("/login");
+// await expect(page).toHaveTitle(/login/i);
+// });
