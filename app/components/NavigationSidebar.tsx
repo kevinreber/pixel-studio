72539d9e239baead602ac8093ae10df3e0446dc5
Kevin Reber <kevinreber1@gmail.com>
2025-02-02 19:59:20 -0500

    added link to likes route in navigation bar

--- app/components/NavigationSidebar.tsx
+++ app/components/NavigationSidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PixelStudioIcon from "components/PixelStudioIcon";
 import { Link } from "@remix-run/react";
-import { Search, Layers, PenTool, User, Images } from "lucide-react";
+import { Search, Layers, PenTool, User, Images, Heart } from "lucide-react";
 import { UserAvatarButton } from "./UserAvatarButton";
 import { useLoggedInUser } from "~/hooks";
 
@@ -47,6 +47,11 @@ const NavigationSidebar = () => {
       title: "Sets",
       icon: <Images className="md:h-4 md:w-4" />,
       href: "/sets",
+    },
+    {
+      title: "Liked",
+      icon: <Heart className="md:h-4 md:w-4" />,
+      href: "/likes",
     },
     // {
     //   title: "Collections",
