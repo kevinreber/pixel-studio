a29542a2d20045a20413e73d65b5c2edfb72bfc0
Kevin Reber <kevinreber1@gmail.com>
2025-02-02 19:58:01 -0500

    created function to get logged in users liked images

---
+++ app/server/getLikedImages.ts
@@ -0,0 +1,77 @@
+import { prisma } from "~/services/prisma.server";
+import { getS3BucketThumbnailURL, getS3BucketURL } from "~/utils/s3Utils";
+
+export const getLikedImages = async (userId: string) => {
+  const likedImages = await prisma.imageLike.findMany({
+    where: {
+      userId: userId,
+    },
+    include: {
+      image: {
+        select: {
+          id: true,
+          title: true,
+          prompt: true,
+          setId: true,
+          model: true,
+          stylePreset: true,
+          private: true,
+          createdAt: true,
+          user: {
+            select: {
+              username: true,
+            },
+          },
+          _count: {
+            select: {
+              likes: true,
+              comments: true,
+            },
+          },
+          comments: {
+            orderBy: {
+              createdAt: "desc",
+            },
+            select: {
+              id: true,
+              message: true,
+              createdAt: true,
+              updatedAt: true,
+              user: {
+                select: {
+                  id: true,
+                  username: true,
+                  image: true,
+                },
+              },
+              parentId: true,
+              likes: true,
+            },
+          },
+          likes: {
+            select: {
+              userId: true,
+            },
+          },
+        },
+      },
+    },
+    orderBy: {
+      image: {
+        createdAt: "desc",
+      },
+    },
+  });
+
+  const formattedImages = likedImages.map((like) => ({
+    ...like.image,
+    thumbnailURL: getS3BucketThumbnailURL(like.image.id),
+    url: getS3BucketURL(like.image.id),
+  }));
+
+  return formattedImages;
+};
+
+export type GetLikedImagesAPIResponse = Awaited<
+  ReturnType<typeof getLikedImages>
+>;
